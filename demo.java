4dc48192245a4f0eb481e20cae82590d36a861d1
SHAMKUMAR M <shamk7765@gmail.com>
2026-01-09 20:32:19 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,42 @@
+package hello;
+
+public class demo {
+public static void main(String[] args) {
+	//Opereatores 
+	int a=100; 
+	int b=20;
+	System.out.println("a+b="+(a+b));
+	System.out.println("a-b="+(a-b));
+	System.out.println("a*b="+(a*b));
+	System.out.println("a/b="+(a/b));
+	System.out.println("a%b="+(a%b));
+	System.out.println(10+ " "+ 20);
+	System.out.println(10+20);
+	System.out.println(10+"\n"+20);
+	System.out.println(a);
+	a+=1;// a=a+1;
+	    System.out.println(a);
+	    a-=1;
+	    System.out.println(a);
+	    a*=1;
+	    System.out.println(a);
+	    a%=1;
+	    System.out.println(a);
+    a=5;
+    b=3;
+    System.out.println(a>b);
+    System.out.println(a<b);
+    System.out.println(a>=b);
+    System.out.println(a<=b);
+    System.out.println(a==b);
+    System.out.println(a!=b);
+	     int Tamil=50;
+	     int English=66;
+	     int Maths=88;
+	     int Science=99;
+	     int social=100;
+	     double avg=(Tamil+English+Maths+Science+social)/5;
+	     System.out.println("Average "+avg);
+	
+}
+}
