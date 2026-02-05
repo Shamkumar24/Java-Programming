fc31fdcb18777de6350b53b147f846775edbf0a1
SHAMKUMAR M <shamk7765@gmail.com>
2026-02-05 06:03:40 +0530

    Create RightAngledTrianglePattern.java

---
+++ Practicing-Programs/Basic-Logic-Math-Based-Coding/Arrays-Patterns10-20/RightAngledTrianglePattern.java
@@ -0,0 +1,20 @@
+//15. Print a right-angled triangle pattern of stars. 
+
+import java.util.Scanner;
+
+public class RightAngledTrianglePattern {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
